0ac8c4cfcf72f1687a42cc84af141b159a3e5af7
Nathan de Moura Godoy <nathanmgodoy@gmail.com>
2021-12-26 12:29:50 -0300

    Add files via upload
    Write a method hasEqualSum with 3 parameters of type int.
    
    The method should return boolean and it needs to return true if the sum of the first and second parameter is equal to the third parameter. Otherwise, return false.
    
    
    EXAMPLES OF INPUT/OUTPUT:
    
    * hasEqualSum(1, 1, 1);  should return false since 1 + 1 is not equal to 1
    
    * hasEqualSum(1, 1, 2);  should return true since 1 + 1 is equal to 2
    
    * hasEqualSum(1, -1, 0);  should return true since 1 + (-1) is 1 - 1 and is equal to 0
    
    
    NOTE: The hasEqualSum method  needs to be defined as public static like we have been doing so far in the course.
    NOTE: Do not add a  main method to solution code.

---
+++ EqualSumChecker/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    System.out.println(EqualSum.hasEqualSum(1,2,3));
+    System.out.println(EqualSum.hasEqualSum(1,3,3));
+    System.out.println(EqualSum.hasEqualSum(2,2,4));
+    }
+}
